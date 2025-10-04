6b3e585014d968bbe4e87835cf9382d5fb75690b
Sarim <sarimyx@proton.me>
2025-10-04 13:25:26 -0600

    comment out

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import { NotificationReceivers } from "./notification-receivers";
 import { sendPrayerTimesNotifications } from "./utils/send-prayer-times-notifications";
-import { sendDailyVerseNotifications } from './utils/send-daily-verse-notifications';
-import { sendDailyChapterNotifications } from './utils/send-daily-chapter-notifications';
+// import { sendDailyVerseNotifications } from './utils/send-daily-verse-notifications';
+// import { sendDailyChapterNotifications } from './utils/send-daily-chapter-notifications';
 import { Server } from './api/server';
 
 (async () => {
@@ -24,33 +24,33 @@ import { Server } from './api/server';
         }
     };
 
-    // const handleDailyVerseNotifications = async () => {
-    //     try {
-    //         const receivers = await notificationReceivers.getReceivers();
+    const handleDailyVerseNotifications = async () => {
+        // try {
+        //     const receivers = await notificationReceivers.getReceivers();
 
-    //         for (const receiver of receivers.filter(receiver => receiver.platform === 'ios')) {
-    //             if (receiver.daily_verse_notifications && receiver.device_token) {
-    //                 await sendDailyVerseNotifications(receiver);
-    //             }
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+        //     for (const receiver of receivers.filter(receiver => receiver.platform === 'ios')) {
+        //         if (receiver.daily_verse_notifications && receiver.device_token) {
+        //             await sendDailyVerseNotifications(receiver);
+        //         }
+        //     }
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    };
 
-    // const handleDailyChapterNotifications = async () => {
-    //     try {
-    //         const receivers = await notificationReceivers.getReceivers();
+    const handleDailyChapterNotifications = async () => {
+        // try {
+        //     const receivers = await notificationReceivers.getReceivers();
 
-    //         for (const receiver of receivers.filter(receiver => receiver.platform === 'ios')) {
-    //             if (receiver.daily_chapter_notifications && receiver.device_token) {
-    //                 await sendDailyChapterNotifications(receiver);
-    //             }
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+        //     for (const receiver of receivers.filter(receiver => receiver.platform === 'ios')) {
+        //         if (receiver.daily_chapter_notifications && receiver.device_token) {
+        //             await sendDailyChapterNotifications(receiver);
+        //         }
+        //     }
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    };
 
     try {
         console.log('Initializing notification receivers...');
@@ -63,17 +63,17 @@ import { Server } from './api/server';
             );
         }, 60000); // 1 minute
         
-        // setInterval(() => {
-        //     handleDailyVerseNotifications().catch(err => 
-        //         console.error('Unhandled error in daily verse notifications interval:', err)
-        //     );
-        // }, 1000 * 60 * 60 * 1); // 1 hours
+        setInterval(() => {
+            handleDailyVerseNotifications().catch(err => 
+                console.error('Unhandled error in daily verse notifications interval:', err)
+            );
+        }, 1000 * 60 * 60 * 1); // 1 hours
         
-        // setInterval(() => {
-        //     handleDailyChapterNotifications().catch(err => 
-        //         console.error('Unhandled error in daily chapter notifications interval:', err)
-        //     );
-        // }, 1000 * 60 * 60 * 3); // 3 hours
+        setInterval(() => {
+            handleDailyChapterNotifications().catch(err => 
+                console.error('Unhandled error in daily chapter notifications interval:', err)
+            );
+        }, 1000 * 60 * 60 * 3); // 3 hours
         
         console.log('✅ Notifications Service is running');
 
